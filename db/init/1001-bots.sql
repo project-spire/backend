ed4fc183e245fcf95c1238b840ec766ad10443a1
Sanito Gonzalez <ddalkisoup23@gmail.com>
2025-02-08 13:41:36 +0900

    Fix DB init queries

--- db/init/1001-bots.sql
+++ db/init/1001-bots.sql
@@ -2,7 +2,7 @@ CREATE TABLE bots
 (
     bot_id     INT PRIMARY KEY,
     account_id INT NOT NULL,
-    created    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (account_id) REFERENCES accounts (account_id)
 );
