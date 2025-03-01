c0d66157f6edecfa8eb0938fc0c4bb558c98e779
SanitoGonzalez <ddalkisoup23@gmail.com>
2025-03-01 15:55:43 +0900

    [update]

--- secrets/generate.sh
+++ secrets/generate.sh
@@ -9,3 +9,5 @@ openssl req -x509 -newkey rsa:4096 -keyout lobby-key.pem -out lobby-cert.pem \
     -sha256 -days 3650 -nodes \
     -subj "/CN=localhost" \
     -addext "subjectAltName=DNS:localhost"
+
+openssl rand -base64 64 > auth-key.txt
