e37f6913f13eaecbe4dffddc37a1879b7ddc205f
SanitoGonzalez <ddalkisoup23@gmail.com>
2025-04-19 16:09:05 +0900

    Add character skills table

---
+++ db/init/2003-character_skills.sql
@@ -0,0 +1,8 @@
+CREATE TABLE character_skills (
+    character_id bigint not null references characters(id),
+    skill_id     bigint not null,
+
+    level integer not null default 1,
+
+    primary key(character_id, skill_id)
+);
