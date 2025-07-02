493af74e687bf11764609f7f072fbd923bdb7245
SanitoGonzalez <ddalkisoup23@gmail.com>
2025-07-02 22:38:15 +0900

    [game-server] Add Session retrieving logic

--- game-server/server/src/net/session.rs
+++ game-server/server/src/net/session.rs
@@ -1,123 +1,167 @@
-use actix::AsyncContext;
+use crate::world::zone::Zone;
+use actix::{Actor, AsyncContext};
 use bevy_ecs::component::Component;
 use bytes::{Bytes, BytesMut};
-use crate::world::zone::Zone;
-use protocol::{HEADER_SIZE, ProtocolCategory, deserialize_header};
-use std::sync::Arc;
-use std::sync::atomic::AtomicBool;
+use protocol::{deserialize_header, ProtocolCategory, HEADER_SIZE};
 use tokio::io::{AsyncReadExt, AsyncWriteExt, ReadHalf, WriteHalf};
 use tokio::net::TcpStream;
-use tokio::sync::{mpsc, Mutex};
+use tokio::sync::{broadcast, mpsc, oneshot};
 
-pub type InMessage = (SessionContext, ProtocolCategory, Bytes);
-pub type OutMessage = Bytes;
+const EGRESS_MESSAGE_BUFFER_SIZE: usize = 16;
 
-#[derive(Clone)]
+pub type IngressMessage = (SessionContext, ProtocolCategory, Bytes);
+pub type EgressMessage = Bytes;
+
+#[derive(Clone, Component)]
 pub struct SessionContext {
-    out_message_tx: mpsc::Sender<OutMessage>,
-    is_stopped: Arc<AtomicBool>,
+    egress_msg_tx: mpsc::Sender<EgressMessage>,
 }
 
 impl SessionContext {
-    pub fn start(&mut self) {
-        self.is_stopped.store(true, std::sync::atomic::Ordering::Relaxed);
+    pub async fn send(&mut self, msg: EgressMessage) {
+        _ = self.egress_msg_tx.send(msg).await;
+    }
+}
+
+pub struct SessionRetriever {
+    retrieve_tx: broadcast::Sender<()>,
+    retrieve_rx: broadcast::Receiver<()>,
+
+    egress_msg_tx: Option<oneshot::Sender<mpsc::Sender<EgressMessage>>>,
+    egress_msg_rx: oneshot::Receiver<mpsc::Receiver<EgressMessage>>,
+
+    reader_tx: Option<oneshot::Sender<ReadHalf<TcpStream>>>,
+    reader_rx: oneshot::Receiver<ReadHalf<TcpStream>>,
+
+    writer_tx: Option<oneshot::Sender<WriteHalf<TcpStream>>>,
+    writer_rx: oneshot::Receiver<WriteHalf<TcpStream>>,
+}
+
+impl SessionRetriever {
+    pub fn new() -> Self {
+        let (retrieve_tx, retrieve_rx) = broadcast::channel(1);
+        let (egress_msg_tx, egress_msg_rx) = oneshot::channel();
+        let (reader_tx, reader_rx) = oneshot::channel();
+        let (writer_tx, writer_rx) = oneshot::channel();
+
+        SessionRetriever {
+            retrieve_tx,
+            retrieve_rx,
+            reader_tx: Some(reader_tx),
+            reader_rx,
+            egress_msg_tx: Some(egress_msg_tx),
+            egress_msg_rx,
+            writer_tx: Some(writer_tx),
+            writer_rx,
+        }
     }
 
-    pub fn stop(&mut self) {
-        self.is_stopped.store(false, std::sync::atomic::Ordering::Relaxed);
+    pub fn take(
+        &mut self,
+    ) -> (
+        oneshot::Sender<mpsc::Sender<EgressMessage>>,
+        oneshot::Sender<ReadHalf<TcpStream>>,
+        oneshot::Sender<WriteHalf<TcpStream>>,
+    ) {
+        (
+            self.egress_msg_tx.take().unwrap(),
+            self.reader_tx.take().unwrap(),
+            self.writer_tx.take().unwrap(),
+        )
     }
 
-    pub async fn send(&mut self, msg: OutMessage) {
-        _ = self.out_message_tx.send(msg).await;
+    pub async fn retrieve(&self) {
+        self.retrieve_tx.send(()).await;
     }
 }
 
 #[derive(Component)]
 pub struct Session {
-    reader: Arc<Mutex<Option<ReadHalf<TcpStream>>>>,
-    writer: Arc<Mutex<Option<WriteHalf<TcpStream>>>>,
+    egress_msg_tx: mpsc::Sender<EgressMessage>,
+    egress_msg_rx: Option<mpsc::Receiver<EgressMessage>>,
 
-    out_message_tx: mpsc::Sender<OutMessage>,
-    out_message_rx: Arc<Mutex<Option<mpsc::Receiver<OutMessage>>>>,
-
-    is_stopped: Arc<AtomicBool>,
+    retriever: SessionRetriever,
 }
 
 impl Session {
     pub fn new() -> Self {
-        let (out_message_tx, out_message_rx) = mpsc::channel::<OutMessage>(todo!("const 가져오기"));
-        
+        let (egress_msg_tx, egress_msg_rx) = mpsc::channel(EGRESS_MESSAGE_BUFFER_SIZE);
+
         Session {
-            reader: Arc::new(Mutex::new(None)),
-            writer: Arc::new(Mutex::new(None)),
-            out_message_tx,
-            out_message_rx: Arc::new(Mutex::new(Some(out_message_rx))),
-            is_stopped: Arc::new(AtomicBool::new(true)),
+            egress_msg_tx,
+            egress_msg_rx: Some(egress_msg_rx),
+            retriever: SessionRetriever::new(),
+        }
+    }
+
+    pub fn new_ctx(&self) -> SessionContext {
+        SessionContext {
+            egress_msg_tx: self.egress_msg_tx.clone(),
         }
     }
 
     pub fn start(
         &mut self,
         socket: TcpStream,
-        in_message_tx: mpsc::Sender<InMessage>,
-        zone: &mut dyn AsyncContext<Zone>
+        ingress_msg_tx: mpsc::Sender<IngressMessage>,
+        zone: &mut dyn AsyncContext<Zone>,
     ) {
-        // if !self.is_stopped.compare_exchange(true, false, std::sync::atomic::Ordering::Relaxed) {
-        //     return;
-        // }
-        let is_stopped = self.is_stopped.clone();
-
         let (mut reader, mut writer) = tokio::io::split(socket);
-        let reader_return = self.reader.clone();
-        let writer_return = self.writer.clone();
-        
-        let out_message_rx_return = self.out_message_rx.clone();
-
-        let ctx = SessionContext {
-            out_message_tx: self.out_message_tx.clone(),
-            is_stopped: is_stopped.clone(),
-        };
+        let mut egress_msg_rx = self
+            .egress_msg_rx
+            .take()
+            .expect("out-message channel must be set before start");
+        let (mut egress_msg_retriever, mut reader_retriever, mut writer_retriever)
+            = self.retriever.take();
 
         // Start receiving-loop
+        let mut retrieve_rx = self.retriever.retrieve_tx.subscribe();
+        let ctx = self.new_ctx();
+
         zone.spawn(async move {
             loop {
-                if is_stopped.load(std::sync::atomic::Ordering::Relaxed) {
-                    break;
-                }
+                let mut header_buf = [0u8; HEADER_SIZE];
+                let mut body_buf = Vec::new();
 
                 if let Err(e) = Self::recv(&mut reader) {
                     eprintln!("Error receiving: {}", e);
+                    //todo: Stop the session
                     break;
                 }
             }
-
-            *reader_return.lock().await = Some(reader);
         });
 
         // Start sending-loop
+        let mut retrieve_rx = self.retriever.retrieve_tx.subscribe();
+
         zone.spawn(async move {
-            let out_message_rx = (*out_message_rx_return.lock().await)
-                .take()
-                .expect("OutMessage channel must be set before start");
-            
+            let mut egress_msg_buf = Vec::with_capacity(EGRESS_MESSAGE_BUFFER_SIZE);
+
             loop {
-                if is_stopped.load(std::sync::atomic::Ordering::Relaxed) {
+                if let Some(_) = retrieve_rx.try_recv() {
                     break;
                 }
 
-                //todo
+                egress_msg_rx
+                    .recv_many(&mut egress_msg_buf, EGRESS_MESSAGE_BUFFER_SIZE)
+                    .await;
+                for msg in egress_msg_buf.drain(..) {
+                    if let Err(e) = Session::send(&mut writer, msg).await {
+                        eprintln!("Error sending: {}", e);
+                        //todo: Stop the session
+                        break;
+                    }
+                }
+                
             }
-
-            *writer_return.lock().await = Some(writer);
-            *out_message_rx_return.lock().await = Some(out_message_rx);
+            
+            egress_msg_retriever.send(egress_msg_rx).await;
         });
     }
 
-    pub fn stop(&mut self) {
-        self.is_stopped.store(false, std::sync::atomic::Ordering::Relaxed);
-    }
-
-    pub async fn recv(reader: &mut ReadHalf<TcpStream>) -> Result<(ProtocolCategory, Bytes), std::io::Error> {
+    pub async fn recv(
+        reader: &mut ReadHalf<TcpStream>,
+    ) -> Result<(ProtocolCategory, Bytes), std::io::Error> {
         let mut header_buf = [0u8; HEADER_SIZE];
         reader.read_exact(&mut header_buf).await?;
         let header = deserialize_header(&header_buf);
@@ -128,7 +172,10 @@ impl Session {
         Ok((header.category, body_buf.freeze()))
     }
 
-    pub async fn send(writer: &mut WriteHalf<TcpStream>, buffer: Bytes) -> Result<(), std::io::Error> {
+    pub async fn send(
+        writer: &mut WriteHalf<TcpStream>,
+        buffer: Bytes,
+    ) -> Result<(), std::io::Error> {
         writer.write_all(&buffer[..]).await?;
         Ok(())
     }
--- game-server/server/src/world/zone.rs
+++ game-server/server/src/world/zone.rs
@@ -1,60 +1,59 @@
 use actix::{Actor, ActorFutureExt, AsyncContext, Context, WrapFuture};
+use crate::net::session::IngressMessage;
 use std::time::Duration;
 use tokio::sync::mpsc;
 
-const IN_MESSAGE_BUFFER_SIZE: usize = 64;
-const OUT_MESSAGE_BUFFER_SIZE: usize = 16;
+const INGRESS_MESSAGE_BUFFER_SIZE: usize = 64;
 const TICK_INTERVAL: Duration = Duration::from_millis(100);
 
 pub struct Zone {
-    in_message_tx: mpsc::Sender<()>,
-    in_message_rx: Option<mpsc::Receiver<()>>,
+    ingress_msg_tx: mpsc::Sender<IngressMessage>,
+    ingress_msg_rx: Option<mpsc::Receiver<IngressMessage>>,
     ticks: u64,
 }
 
 impl Zone {
     pub fn new() -> Self {
-        let (in_message_tx, in_message_rx) = mpsc::channel(IN_MESSAGE_BUFFER_SIZE);
-        let in_message_rx = Some(in_message_rx);
+        let (ingress_msg_tx, ingress_msg_rx) = mpsc::channel(INGRESS_MESSAGE_BUFFER_SIZE);
 
         Zone {
-            in_message_tx,
-            in_message_rx,
+            ingress_msg_tx,
+            ingress_msg_rx: Some(ingress_msg_rx),
             ticks: 0,
         }
     }
 
-    fn handle_in_messages(
+    fn handle_ingress_messages(
         &self,
         ctx: &mut <Self as Actor>::Context,
-        mut in_message_rx: mpsc::Receiver<()>,
+        mut ingress_msg_rx: mpsc::Receiver<IngressMessage>,
     ) {
         // Start a message handling recursion
         ctx.spawn(
             async move {
-                let mut in_messages = Vec::with_capacity(IN_MESSAGE_BUFFER_SIZE);
-                _ = in_message_rx
-                    .recv_many(&mut in_messages, IN_MESSAGE_BUFFER_SIZE)
+                let mut ingress_msg_buf = Vec::with_capacity(INGRESS_MESSAGE_BUFFER_SIZE);
+                _ = ingress_msg_rx
+                    .recv_many(&mut ingress_msg_buf, INGRESS_MESSAGE_BUFFER_SIZE)
                     .await;
 
-                (in_message_rx, in_messages)
+                (ingress_msg_rx, ingress_msg_buf)
             }
             .into_actor(self)
             .then(|res, act, ctx| {
-                let (in_message_rx, mut in_messages) = res;
-                for in_message in in_messages.drain(..) {
-                    act.handle_in_message(ctx, in_message);
+                let (in_message_rx, mut msg_buf) = res;
+                for msg in msg_buf.drain(..) {
+                    act.handle_ingress_message(ctx, msg);
                 }
 
                 // Recursion without starving Actor's tick task
-                act.handle_in_messages(ctx, in_message_rx);
+                act.handle_ingress_messages(ctx, in_message_rx);
 
                 actix::fut::ready(())
             }),
         );
     }
 
-    fn handle_in_message(&mut self, ctx: &mut <Self as Actor>::Context, in_message: ()) {}
+    fn handle_ingress_message(&mut self, ctx: &mut <Self as Actor>::Context, msg: IngressMessage) {}
 
     fn tick(&mut self, ctx: &mut <Self as Actor>::Context) {
         self.ticks += 1;
@@ -65,12 +64,12 @@ impl Actor for Zone {
     type Context = Context<Self>;
 
     fn started(&mut self, ctx: &mut Self::Context) {
-        let in_message_rx = self
-            .in_message_rx
+        let ingress_msg_rx = self
+            .ingress_msg_rx
             .take()
             .expect("InMessage channel should be set before start");
 
-        self.handle_in_messages(ctx, in_message_rx);
+        self.handle_ingress_messages(ctx, ingress_msg_rx);
 
         ctx.run_interval(TICK_INTERVAL, |act, ctx| {
             act.tick(ctx);
