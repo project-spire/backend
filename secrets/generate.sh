c65ecaabea4f15a5ac4e0458d3ad178b140f1638
SanitoGonzalez <ddalkisoup23@gmail.com>
2025-08-31 18:12:50 +0900

    [backend] Add db-password generation guard and show logs

--- secrets/generate.sh
+++ secrets/generate.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
 
+set -e
+
 # Game Server Certificate
-openssl req -x509 -newkey rsa:4096 -keyout game-server-key.pem -out game-server-cert.pem \
-    -sha256 -days 3650 -nodes \
+echo "Generating self-signed game server certificate..."
+openssl req -quiet -x509 -newkey rsa:4096 \
+    -keyout game-server-key.pem -out game-server-cert.pem \
+    -sha256 -days 365 -nodes \
     -subj "/CN=game.spire.dev" \
-    -addext "subjectAltName=DNS:game.spire.dev"
+    -addext "subjectAltName=DNS:game.spire.dev" \
+    2>/dev/null
 
 # Lobby Server Certificate
-openssl req -x509 -newkey rsa:4096 -keyout lobby-server-key.pem -out lobby-server-cert.pem \
-    -sha256 -days 3650 -nodes \
+echo "Generating self-signed lobby server certificate..."
+openssl req -quiet -x509 -newkey rsa:4096 \
+    -keyout lobby-server-key.pem -out lobby-server-cert.pem \
+    -sha256 -days 365 -nodes \
     -subj "/CN=lobby.spire.dev" \
-    -addext "subjectAltName=DNS:lobby.spire.dev"
+    -addext "subjectAltName=DNS:lobby.spire.dev" \
+    2>/dev/null
 
 # DB Certificate
-openssl req -x509 -newkey rsa:4096 -keyout db-key.pem -out db-cert.pem \
-    -sha256 -days 3650 -nodes \
+echo "Generating self-signed DB certificate..."
+openssl req -quiet -x509 -newkey rsa:4096 \
+    -keyout db-key.pem -out db-cert.pem \
+    -sha256 -days 365 -nodes \
     -subj "/CN=db.spire.dev" \
-    -addext "subjectAltName=DNS:db.spire.dev"
+    -addext "subjectAltName=DNS:db.spire.dev" \
+    2>/dev/null
 chmod +r db-key.pem
 
 # Token Key
 openssl rand -base64 32 | head -c -1 > token.key
 
 # DB Password
-echo "password" | head -c -1 > db-password.key
+if [ ! -f db-password.key ]; then
+    echo "Generating DB password..."
+    echo "password" | head -c -1 > db-password.key
+else
+    echo "DB password already exist. Skipping..."
+fi
+
+echo "Generate complete!"
