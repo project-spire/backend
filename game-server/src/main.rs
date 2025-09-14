e7b1ea86a1bb164c65ce0a990933a1921e261317
SanitoGonzalez <ddalkisoup23@gmail.com>
2025-09-14 22:57:56 +0900

    [game-server] Add crypto provider installation

--- game-server/src/main.rs
+++ game-server/src/main.rs
@@ -14,9 +14,11 @@ mod util {
 }
 mod world;
 
+use std::process::exit;
 use actix::prelude::*;
 use clap::Parser;
 use mimalloc::MiMalloc;
+use rustls::crypto::aws_lc_rs;
 use tracing::{info, error};
 use crate::env::Env;
 use crate::net::authenticator::Authenticator;
@@ -38,27 +40,32 @@ struct Options {
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    if let Err(e) = aws_lc_rs::default_provider().install_default() {
+        error!("Failed to install crypto provider: {:?}", e);
+        exit(1);
+    }
+
     let options = Options::parse();
     if let Err(e) = Config::init() {
         error!("Failed to initialize configuration: {e}");
-        return;
+        exit(1);
     }
     if let Err(e) = Env::init() {
         error!("Failed to initialize environment: {e}");
-        return;
+        exit(1);
     }
 
     let db_pool = match db::connect().await {
         Ok(pool) => pool,
         Err(e) => {
             error!("Failed to connect to database: {}", e);
-            return;
+            exit(1);
         },
     };
 
     if let Err(e) = data::load_all(&Env::get().data_dir).await {
         error!("Failed to load data: {}", e);
-        return;
+        exit(1);
     }
 
     let default_zone = Zone::new(0).start();
@@ -70,7 +77,7 @@ async fn main() {
 
     if options.dry_run {
         info!("Dry running done");
-        return;
+        exit(0);
     }
 
     tokio::signal::ctrl_c().await.unwrap();
